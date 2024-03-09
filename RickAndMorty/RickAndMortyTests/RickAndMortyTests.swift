5066ecc9970f5c9ad1724a7935093f4381a36dd6
Yan Moroz <mail@yanmoroz.ru>
2024-03-09 22:41:04 +0300

    Implement simple request example

--- RickAndMorty/RickAndMortyTests/RickAndMortyTests.swift
+++ RickAndMorty/RickAndMortyTests/RickAndMortyTests.swift
@@ -10,4 +10,58 @@ import XCTest
 
 final class RickAndMortyTests: XCTestCase {
     
+    struct Episode: Decodable {
+        let id: Int
+    }
+    
+    func test_simpleRequest() {
+        let fakeExtenralCompletion: (Result<Any, Error>) -> Void = { result in
+            
+        }
+        
+        let expectation = XCTestExpectation()
+        // 0. Build URL or URLRequest
+        let url = URL(string: "https://rickandmortyapi.com/api/episode/5")!
+        // 1. Proceed HTTP request
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            defer {
+                expectation.fulfill()
+            }
+            
+            // 2. Handle error
+            if let error {
+                fakeExtenralCompletion(.failure(error))
+                return
+            }
+            
+            // 3. Validate response
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode > 300 {
+                // ...
+            }
+            
+            // 4. Check data exists ...
+            guard let data else {
+                fakeExtenralCompletion(.failure(NSError(domain: "network", code: 1234)))
+                return
+            }
+            
+            do {
+                // ... and decode to some <T>
+                let episode = try JSONDecoder().decode(Episode.self, from: data)
+                fakeExtenralCompletion(.success(episode))
+            } catch let decodeError {
+                // 5. Handle decode error
+                fakeExtenralCompletion(.failure(decodeError))
+            }
+        }
+        
+        task.resume()
+        wait(for: expectation)
+    }
+}
+
+extension XCTestCase {
+    func wait(for expectation: XCTestExpectation) {
+        wait(for: [expectation])
+    }
 }
