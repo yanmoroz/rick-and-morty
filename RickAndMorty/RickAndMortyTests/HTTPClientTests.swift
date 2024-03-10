603454393b391eb52a60ca724a3a7a6f94b058f3
Yan Moroz <mail@yanmoroz.ru>
2024-03-10 04:15:39 +0300

    Implement test_httpClientMock_returnsError method

--- RickAndMorty/RickAndMortyTests/HTTPClientTests.swift
+++ RickAndMorty/RickAndMortyTests/HTTPClientTests.swift
@@ -13,6 +13,7 @@ final class HTTPClientTests: XCTestCase {
         static let baseUrl = URL(string: "https://mock.api.io")!
         static let request = URLRequest(url: baseUrl)
         static let cancellationError = URLError(URLError.cancelled)
+        static let someError = URLError(URLError.notConnectedToInternet)
     }
     
     func test_httpClientMock_cancels() {
@@ -31,5 +32,22 @@ final class HTTPClientTests: XCTestCase {
         
         task.cancel()
         wait(for: exp)
+    }
+    
+    func test_httpClientMock_returnsError() {
+        let sut = HTTPClientMock()
+        
+        URLProtocolMock.requestHandler = { request in
+            (nil, nil, Locals.someError)
+        }
+        
+        let exp = XCTestExpectation()
+        sut.request(Locals.request) { _, _, error in
+            defer { exp.fulfill() }
+            let error = error as! URLError
+            XCTAssertEqual(error.code, Locals.someError.code)
+        }
+        
+        wait(for: [exp])
     }
 }
