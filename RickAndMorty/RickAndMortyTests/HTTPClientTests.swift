e3b2192661f93df9a5f29becf84657947859c802
Yan Moroz <mail@yanmoroz.ru>
2024-03-10 16:30:28 +0300

    Implement test_httpClientMock_returnsResponse method

--- RickAndMorty/RickAndMortyTests/HTTPClientTests.swift
+++ RickAndMorty/RickAndMortyTests/HTTPClientTests.swift
@@ -14,6 +14,8 @@ final class HTTPClientTests: XCTestCase {
         static let request = URLRequest(url: baseUrl)
         static let cancellationError = URLError(URLError.cancelled)
         static let someError = URLError(URLError.notConnectedToInternet)
+        static let badStatusCode = 404
+        static let response = HTTPURLResponse(url: baseUrl, statusCode: badStatusCode, httpVersion: nil, headerFields: nil)
     }
     
     func test_httpClientMock_cancels() {
@@ -48,6 +50,26 @@ final class HTTPClientTests: XCTestCase {
             XCTAssertEqual(error.code, Locals.someError.code)
         }
         
-        wait(for: [exp])
+        wait(for: exp)
+    }
+    
+    func test_httpClientMock_returnsResponse() {
+        let sut = HTTPClientMock()
+        
+        URLProtocolMock.requestHandler = { request in
+            (nil, Locals.response, nil)
+        }
+        
+        let exp = XCTestExpectation()
+        sut.request(Locals.request) { _, response, _ in
+            defer { exp.fulfill() }
+            guard let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == Locals.badStatusCode else {
+                XCTFail("Shouldn't fail")
+                return
+            }
+        }
+        
+        wait(for: exp)
     }
 }
