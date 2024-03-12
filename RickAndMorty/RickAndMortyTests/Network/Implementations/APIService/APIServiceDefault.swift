819f3fb8818744f540e80ff3e542cf9cbec72cb0
Yan Moroz <mail@yanmoroz.ru>
2024-03-12 23:05:41 +0300

    Implement APIServiceDefault

--- RickAndMorty/RickAndMortyTests/Network/Implementations/APIService/APIServiceDefault.swift
+++ RickAndMorty/RickAndMortyTests/Network/Implementations/APIService/APIServiceDefault.swift
@@ -13,7 +13,18 @@ struct APIServiceDefault: APIService {
     func request<T: Decodable, Request: DecodableAPIRequest>(_ request: Request, completion: @escaping Completion<T>)
     -> CancellableTask where T == Request.DecodeTargetType {
         networkService.request(request) { result in
-            
+            switch result {
+            case .success(let data):
+                let decodeResult = request.decoder.decode(data, toType: T.self)
+                switch decodeResult {
+                case .success(let decoded):
+                    completion(.success(decoded))
+                case .failure(let error):
+                    completion(.failure(APIServiceError(error)))
+                }
+            case .failure(let networkServiceError):
+                completion(.failure(APIServiceError(networkServiceError)))
+            }
         }
     }
 }
