04f249b2ebf5afedfc8400fd7486bc996517472f
Yan Moroz <mail@yanmoroz.ru>
2024-04-05 18:56:00 +0300

    Add simple dataTask example to pg

--- RickAndMorty/AppPlayground.playground/Contents.swift
+++ RickAndMorty/AppPlayground.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+import Foundation
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
 
-var greeting = "Hello, playground"
+let urlRequest = URLRequest(url: URL(string: "https://foo.bar")!)
+URLSession.shared.dataTask(with: urlRequest) { data, _, _ in
+    // handle error and validate response
+    // Data -> some T
+    if let data {
+        let foo = try? JSONDecoder().decode(Foo.self, from: data)
+        // completion(...)
+    }
+}
+
+struct Foo: Decodable {
+    
+}
