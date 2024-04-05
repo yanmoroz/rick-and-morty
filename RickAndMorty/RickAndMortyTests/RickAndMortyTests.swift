8e90e92ce4c7e9c8f14fadad1fbab846da3d45ca
Yan Moroz <mail@yanmoroz.ru>
2024-04-05 23:51:11 +0300

    Implement test_apiService_returnsResourcesResponse method

--- RickAndMorty/RickAndMortyTests/RickAndMortyTests.swift
+++ RickAndMorty/RickAndMortyTests/RickAndMortyTests.swift
@@ -8,5 +8,24 @@
 import XCTest
 
 final class RickAndMortyTests: XCTestCase {
-    
+    func test_apiService_returnsResourcesResponse() {
+        let networkService = NetworkServiceImpl()
+        let apiService = APIServiceImpl(networkService: networkService)
+        let urlRequest = URLRequest(url: URL(string: "https://rickandmortyapi.com/api")!)
+        let endpoint = EndpointImpl<ResourcesResponse>(urlRequest: urlRequest)
+        let exp = XCTestExpectation()
+        
+        apiService.request(endpoint) { result in
+            defer { exp.fulfill() }
+            
+            switch result {
+            case .success(let response):
+                print(response)
+            case .failure(let apiServiceError):
+                print(apiServiceError)
+            }
+        }
+        
+        wait(for: [exp])
+    }
 }
