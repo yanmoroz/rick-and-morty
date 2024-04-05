613e997eabb8dc93222aa0e11cb0ec4806fc0bf7
Yan Moroz <mail@yanmoroz.ru>
2024-04-05 19:06:02 +0300

    Define APIService with request<T>(...) method Get compile error: "Generic parameter 'T' could not be inferred"

--- RickAndMorty/AppPlayground.playground/Contents.swift
+++ RickAndMorty/AppPlayground.playground/Contents.swift
@@ -2,16 +2,41 @@ import Foundation
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-let urlRequest = URLRequest(url: URL(string: "https://foo.bar")!)
-URLSession.shared.dataTask(with: urlRequest) { data, _, _ in
-    // handle error and validate response
-    // Data -> some T
-    if let data {
-        let foo = try? JSONDecoder().decode(Foo.self, from: data)
-        // completion(...)
+func execute(_ action: @escaping () -> Void) {
+    action()
+}
+
+execute {
+    let urlRequest = URLRequest(url: URL(string: "https://foo.bar")!)
+    URLSession.shared.dataTask(with: urlRequest) { data, _, _ in
+        // handle error and validate response
+        // Data -> some T
+        if let data {
+            let foo = try? JSONDecoder().decode(Foo.self, from: data)
+            // completion(...)
+        }
     }
 }
 
 struct Foo: Decodable {
     
+}
+
+protocol APIService {
+    typealias Completion<T> = (Result<T, Error>) -> Void
+    func request<T>(_ urlRequest: URLRequest, completion: @escaping Completion<T>)
+}
+
+class APIServiceImpl: APIService {
+    func request<T>(_ urlRequest: URLRequest, completion: @escaping Completion<T>) {
+        // ...
+    }
+}
+
+execute {
+    let apiService = APIServiceImpl()
+    let urlRequest = URLRequest(url: URL(string: "https://foo.bar")!)
+    apiService.request(urlRequest) { result in
+        // ...
+    }
 }
